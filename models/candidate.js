f4651a5d63af6b9f617f4ff6cf6245a50108207d
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2026-06-22 09:19:03 +0100

    Latest Docs

--- models/candidate.js
+++ models/candidate.js
@@ -1,47 +1,61 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const candidateSchema = new mongoose.Schema({
+const candidateSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        min: 3,
-        max: 255
+      type: String,
+      required: true,
+      min: 3,
+      max: 255,
     },
     otherName: {
-        type: String,
+      type: String,
     },
     photo: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     nickname: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     post: {
-        type: String,
-        required: true,
-        enum: ['President', 'Vice President', 'General Secretary', 'Financial Secretary', 'Welfare Secretary', 'Treasurer', 'Social Secretary', 'Assistant General Secretary', 'PRO', 'Sports Secretary', 'Technical Director', 'SRC', 'Librarian'],
+      type: String,
+      required: true,
+      enum: [
+        "President",
+        "Vice President",
+        "General Secretary",
+        "Financial Secretary",
+        "Welfare Secretary",
+        "Treasurer",
+        "Social Secretary",
+        "Assistant General Secretary",
+        "PRO",
+        "Sports Secretary",
+        "Technical Director",
+        "SRC",
+        "Librarian",
+      ],
     },
     department: {
-        type: String,
-        required: true,
-        enum: ['ABE', 'BME', 'CHE', 'CPE', 'CVE', 'ELE', 'FBE', 'MEE', 'MME', 'WRE']
+      type: String,
+      required: true,
     },
-    level: {
+
+    voters: [
+      {
         type: String,
-        required: false,
-        enum: ['100', '200', '300', '400']
-    },
-    voters: [{
-        type: String,
-    }],
+      },
+    ],
     votes: {
-        type: Number,
-        default: 0,
+      type: Number,
+      default: 0,
     },
-}, { timestamps: true })
+  },
+  { timestamps: true },
+);
 
-const Candidate = mongoose.model('Candidate', candidateSchema)
+const Candidate = mongoose.model("Candidate", candidateSchema);
 
-module.exports = Candidate
+module.exports = Candidate;
